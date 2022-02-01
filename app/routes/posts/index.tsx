233a20f0ce6bdc7c7a2bc31dcae376327a0d96f3
kayac-chang <egg734631@gmail.com>
2022-02-01 13:39:10 +0800

    Initial commit

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,23 @@
+import { useLoaderData, Link } from "remix";
+import { getPosts } from "~/post";
+import type { Post } from "~/post";
+
+export const loader = async () => {
+  return getPosts();
+};
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+  return (
+    <div>
+      <h1>Post</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ posts/test21.md
@@ -0,0 +1,5 @@
+---
+title: test21
+---
+
+test21
