03a4b830c5e77bfcaec2d6da190216318940027c
Kimberly <work223305@gmail.com>
2022-02-02 00:12:13 +0800

    Add title linear ,and color theme

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,8 @@ import { Link } from "remix";
 
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
+    <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-lavender to-violets">
+      Product
+    </h1>
   );
 }
