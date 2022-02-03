126e631ff02a31ad13215cb2466dedca8cd20bf7
Kimberly <work223305@gmail.com>
2022-02-03 16:10:42 +0800

    index layout finished.(font import faild, icon use absolute path ,li-disc color change faild.)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,43 @@
+import info from "/Users/yantingfen/Developer/fp-dreamland/src/assets/svgs/info.svg";
+import more from "/Users/yantingfen/Developer/fp-dreamland/src/assets/svgs/more.svg";
+
+// import info from "@src/assets/info.svg";
+// import info from "@src/assets/more.svg";
+
 export default function Index() {
   return (
-    <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-lavender to-violets">
-      Product
-    </h1>
+    <html className=" bg-fp_black">
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-medium text-transparent bg-clip-text bg-gradient-to-bl from-lavender to-violets">
+          Product
+        </h1>
+
+        <div className="flex">
+          <img src={info} alt="info" />
+          <p className="text-fp_gray font-Rubik px-4 leading-loose">
+            Updated 3 hours ago
+          </p>
+          <img src={more} alt="more" />
+        </div>
+
+        <p className="text-white pt-9">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          ut dignissim sapien. Vivamus convallis massa sit amet justo varius
+          ornare. Cras nibh tortor, tristique sit amet tortor nec, ullamcorper
+          euismod enim. Aliquam tortor diam, pharetra in varius sit amet, mollis
+          ut massa.
+        </p>
+
+        {/* image */}
+
+        <h2 className="text-white text-2xl">
+          Efficient, lightweight and simple
+        </h2>
+        <ul className="text-white list-disc ml-5 ">
+          <li>Intuitive and quick to use</li>
+          <li>Structure your content the way you need it</li>
+        </ul>
+      </div>
+    </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        fp_black: "#272523",
+        fp_gray: "#A5A4A9",
         lavender: "#BF95F3",
         violets: "#9B7BFA",
+      },
+      fontFamily: {
+        Rubik: ["'Rubik'"],
+      },
+      listStyleType: {
+        none: "none",
+        disc: "disc",
+        decimal: "decimal",
+        square: "square",
+        roman: "upper-roman",
       },
     },
   },
